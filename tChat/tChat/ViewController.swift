375246a3997a8c37b43560b7feb0d21d1f8a0a33
Aibol Tungatarov <aibolseed@gmail.com>
2019-07-25 10:29:53 +0600

    created xcode project, included unit and ui tests and deleted main.storyboard ヅ

---
+++ tChat/tChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tChat
+//
+//  Created by Aibol Tungatarov on 7/25/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
