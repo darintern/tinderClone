23e23e050438e2b25762e644ef5e25700234f1bd
aibol <aibolseed@gmail.com>
2019-07-25 21:20:11 +0600

    added Firebase and SnapKit

---
+++ tChat/tChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tChat
+//
+//  Created by Aibol Tungatarov on 7/25/19.
+//  Copyright © 2019 Aibol Tungatarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
